8d6e5312d9cc1d995f27ad2515312782ee58e855
Rafael Ventura <raf4ael@gmail.com>
2022-11-09 15:27:42 -0300

    Update lista_03.py

--- Lista_03/lista_03.py
+++ Lista_03/lista_03.py
@@ -1,25 +1,13 @@
 # Lista grupo 8
 
-# Ex. 1 - 
-
-# 1. Uma árvore é uma estrutura tipicamente recursiva. 
-# Nós vimos um algoritmo iterativo para realizar busca. 
-# Porque não utilizar um algoritmo recursivo para a busca em uma árvore binária de busca?
+# Ex. 1 - Feito
 
 # Ex. 2 - Feito 
 
-# Ex. 3
-
-# 3. Existe alguma razão para evitarmos algoritmos de busca em 
-#árvore recursivos (memória, complexidade, etc)? Justifique.
-
+# Ex. 3 - Feito
+ 
 # Ex. 4 - Feito 
 
-# Ex. 5
+# Ex. 5 - Anulado
 
-# 5. Elimine os nodos 3 e 12 da árvore do exemplo de deleção em 
-#árvore binária de busca anterior, nesta ordem, a partir da árvore anterior utilizando o algoritmo dado. Mostre o stack a cada chamada recursiva.
-
-# Ex. 6
-
-# 6. Faça um resumo de uma página + exemplos de código de árvore binária de busca, AVL e Rubro-negras
+# Ex. 6 - Feito
